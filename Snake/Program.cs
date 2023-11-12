920f85a83275308418a0398a17d76df7e607e65d
stanislavfor <stasposter@narod.ru>
2023-11-13 01:51:17 +0300

    lesson4

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -18,11 +18,12 @@ namespace Snake
       // char sym1 = '*';
       // Draw(x1, y1, sym1);
 
-      Point p1 = new Point();
-      p1.x = 1;
-      p1.y = 3;
-      p1.sym = '*';
+      // Point p1 = new Point();
+      // p1.x = 1;
+      // p1.y = 3;
+      // p1.sym = '*';
       // Draw(p1.x, p1.y, p1.sym);
+      Point p1 = new Point(1, 3, '*');
       p1.Draw();
 
       // int x2 = 4;
@@ -33,11 +34,12 @@ namespace Snake
       // // Console.ReadLine();
       // Draw(x2, y2, sym2);
 
-      Point p2 = new Point();
-      p2.x = 4;
-      p2.y = 5;
-      p2.sym = '#';
+      // Point p2 = new Point();
+      // p2.x = 4;
+      // p2.y = 5;
+      // p2.sym = '#';
       // Draw(p2.x, p2.y, p2.sym);
+      Point p2 = new Point(4, 5, '#');
       p2.Draw();
 
       // int x3 = 7;
@@ -45,11 +47,12 @@ namespace Snake
       // char sym3 = '*';
       // Draw(x3, y3, sym3);
 
-      Point p3 = new Point();
-      p3.x = 7;
-      p3.y = 8;
-      p3.sym = '*';
+      // Point p3 = new Point();
+      // p3.x = 7;
+      // p3.y = 8;
+      // p3.sym = '*';
       // Draw(p3.x, p3.y, p3.sym);
+      Point p3 = new Point(7, 8, '*');
       p3.Draw();
 
 
